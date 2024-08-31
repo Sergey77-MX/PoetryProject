fabad8514e5dded04816efbe4ca951c0df5e5356
sergey <melnikovs236@gmail.com>
2024-09-01 01:42:07 +0700

    task 9,2 - date function

--- src/widget.py
+++ src/widget.py
@@ -1,5 +1,24 @@
-def mask_account_card():
-    pass
+import masks
+
+def mask_account_card(card: str) -> str:
+    """Функция для обработки информации о картах и счетах.
+    Возвращает замаскированный номер"""
+    numb = ""
+    name = ""
+    for c in card:
+        if c.isdigit():
+            numb += str(c)
+        else:
+            name += str(c)
+    if len(numb) == 16:
+        numb_mask = masks.get_mask_card_number(numb)
+        mask = name + numb_mask
+        return mask
+    else:
+        numb_mask = masks.get_mask_account(numb)
+        mask = name + numb_mask
+        return mask
+
 
 
 def get_date():
