74bdc833fc39e5c7ab10d12496eea5a80b146596
sergey <melnikovs236@gmail.com>
2024-09-06 06:39:43 +0700

    add_processing

--- src/processing.py
+++ src/processing.py
@@ -1,12 +1,12 @@
 from typing import List, Dict, Any
 
 
-def filter_by_state(data: List[Dict[str, Any]], state: str = 'EXECUTED') -> List[Dict[str, Any]]:
+def filter_by_state(data: list, state: str = 'EXECUTED') -> list:
     """Функция фильтрует данные по указанному состоянию"""
     return [d for d in data if d.get('state') == state]
 
 
-def sort_by_date(date_list: list, reverse_list: bool = True) -> list | bool:
+def sort_by_date(date_list: list, reverse_list: bool = True) -> list:
     """Функция сортировки списка словарей по дате"""
     sorted_list = sorted(date_list, key=lambda date_dict: date_dict.get("date"), reverse=reverse_list)
     return sorted_list
