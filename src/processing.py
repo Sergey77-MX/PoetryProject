a9913f918aac279b9fc1b6a18026778011305da9
sergey <melnikovs236@gmail.com>
2024-09-07 19:02:38 +0700

    check_mypy

--- src/processing.py
+++ src/processing.py
@@ -6,7 +6,7 @@ def filter_by_state(data: list[dict[str, Any]], state: str = 'EXECUTED') -> list
     return [d for d in data if d.get('state') == state]
 
 
-def sort_by_date(date_list: list[dict[str, Any]], direction: bool = True) -> list[dict[str, Any]]:
+def sort_by_date(date_list: list, direction: bool = True) -> list[dict[str, Any]]:
     """Функция сортировки списка словарей по дате"""
     sorted_list = sorted(date_list, key=lambda x: x.get("date"), reverse=direction)
     return sorted_list
