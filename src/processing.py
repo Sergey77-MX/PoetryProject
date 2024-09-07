cf3b57483c033b48b0803c6b30584be26efba09a
sergey <melnikovs236@gmail.com>
2024-09-07 20:25:48 +0700

    with improvements

--- src/processing.py
+++ src/processing.py
@@ -1,12 +1,9 @@
-from typing import List, Dict, Any
-
-
-def filter_by_state(data: list[dict[str, Any]], state: str = 'EXECUTED') -> list[dict[str, Any]]:
+def filter_by_state(data: list[dict[str, str]], state: str = 'EXECUTED') -> list[dict[str, str]]:
     """Функция фильтрует данные по указанному состоянию"""
     return [d for d in data if d.get('state') == state]
 
 
-def sort_by_date(date_list: list, direction: bool = True) -> list[dict[str, Any]]:
+def sort_by_date(date_list: list, direction: bool = True) -> list:
     """Функция сортировки списка словарей по дате"""
     sorted_list = sorted(date_list, key=lambda x: x.get("date"), reverse=direction)
     return sorted_list
