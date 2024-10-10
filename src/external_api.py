27c94dd1c0c57e3938c2ca47a138dbadb75f4769
sergey <melnikovs236@gmail.com>
2024-10-10 19:03:01 +0700

    modification 12_1

--- src/external_api.py
+++ src/external_api.py
@@ -1,23 +1,30 @@
-from typing import Any
-
-import requests
 import os
+import requests
+
 from dotenv import load_dotenv
 
-
 load_dotenv()
-values = os.getenv("PASSWORD")
-# keys = os.getenv("API_KEY")
-# headers = {keys: values}
+api_key = os.getenv("API_KEY")
 
 
-def currency_conversion(transaction: Any) -> Any:
-    """Функция конвертации"""
-    amout = transaction["operationAmount"]["amount"]
-    code = transaction["operationAmount"]["currency"]["code"]
-    to = "RUB"
-    url = f"https://api.apilayer.com/exchangerates_data/convert?to={to}&from={code}&amount={amout}"
-    payload = {}
-    response = requests.get(url, headers={"apikey": values}, data=payload)
-    result = response.json()
-    return result["result"]
+def convert_to_rub(transaction_convert):
+    amount = transaction_convert["amount"]
+    currency = transaction_convert["currency"]
+    """Принимает значение в долларах или евро, обращается к внешнему API и возвращает конвертацию в рубли"""
+    try:
+        if currency == "USD":
+            url = f"https://api.apilayer.com/exchangerates_data/convert?to=RUB&from=USD&amount={amount}"
+            headers = {"apikey": api_key}
+            response = requests.get(url, headers=headers)
+            json_result = response.json()
+            rub_amount = json_result["result"]
+            return round(rub_amount, 2)
+        elif currency == "EUR":
+            url = f"https://api.apilayer.com/exchangerates_data/convert?to=RUB&from=EUR&amount={amount}"
+            headers = {"apikey": api_key}
+            response = requests.get(url, headers=headers)
+            json_result = response.json()
+            rub_amount = json_result["result"]
+            return round(rub_amount, 2)
+    except KeyError:
+        return 0
--- tests/test_external_api.py
+++ tests/test_external_api.py
@@ -2,31 +2,42 @@ from unittest.mock import patch
 
 import pytest
 
-from src.external_api import currency_conversion
+from src.external_api import convert_to_rub
 
 
 @pytest.fixture
 def trans_1():
-    return {
-        "id": 441945886,
-        "state": "EXECUTED",
-        "date": "2019-08-26T10:50:58.294041",
-        "operationAmount": {
-            "amount": "31957.58",
-            "currency": {
-                "name": "руб.",
-                "code": "USD"}
-        },
-        "description": "Перевод организации",
-        "from": "Maestro 1596837868705199",
-        "to": "Счет 64686473678894779589"
-    }
+    return {"amount": 92688.46, "currency": "USD"}
+
+
+@pytest.fixture
+def trans_2():
+    return {"amount": 92688.46, "currency": "EUR"}
+
+
+@pytest.fixture
+def trans_3():
+    return {"amount": 0, "currency": "USD"}
 
 
 @patch('requests.get')
 def test_currency_conversion(mock_get, trans_1):
-    mock_get.return_value.json.return_value = {'success': True, 'query':
-                                               {'from': 'USD', 'to': 'RUB', 'amount': 8221.37}, 'info':
-                                               {'timestamp': 1724671757, 'rate': 91.475458},
-                                               'date': '2024-08-26', 'result': 752053.586137}
-    assert currency_conversion(trans_1) == 752053.586137
+    mock_get.return_value.json.return_value = {"success": True, "query":
+                                               {"from": "USD", "to": "RUB", "amount": "8221.37"}, "info":
+                                               {"timestamp": 1724671757, "rate": 91.475458},
+                                               "date": "2024-08-26", "result": 752053.58}
+    assert convert_to_rub(trans_1) == 752053.58
+
+
+@patch('requests.get')
+def test_currency_conversion_2(mock_get, trans_2):
+    mock_get.return_value.json.return_value = {"success": True, "query":
+                                               {"from": "EUR", "to": "RUB", "amount": "8221.37"}, "info":
+                                               {"timestamp": 1724671757, "rate": 91.475458},
+                                               "date": "2024-08-26", "result": 752053.58}
+    assert convert_to_rub(trans_2) == 752053.58
+
+
+@patch('requests.get')
+def test_currency_conversion_3(trans_3):
+    assert convert_to_rub(trans_3) == None
